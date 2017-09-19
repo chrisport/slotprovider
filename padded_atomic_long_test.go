aae2d6e13b302bfb3f403a605a9b83da047adb2d
Johannes Huning <johannes.huning@gmail.com>
2017-09-19 15:11:59 +0200

    add implementation using padded atomic

---
+++ padded_atomic_long_test.go
@@ -0,0 +1,21 @@
+package slotprovider_test
+
+import (
+	"testing"
+	"github.com/johannesh/slotprovider"
+)
+
+func Benchmark_AtomicPaddedLong(b *testing.B) {
+	sp := slotprovider.NewWithAtomicPaddedLong(nrOfSlots)
+	benchmark(b, sp)
+}
+
+func BenchmarkVerify_AtomicPaddedLong_parallel(b *testing.B) {
+	sp := slotprovider.NewWithAtomicPaddedLong(nrOfSlots)
+	verify_parallel(b, sp)
+}
+
+func Benchmark_AtomicPaddedLong_parallel(b *testing.B) {
+	sp := slotprovider.NewWithAtomicPaddedLong(nrOfSlots)
+	benchmark_parallel(b, sp)
+}
